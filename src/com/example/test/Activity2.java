289ea2e29e61bb1df5ea4ac87b67a83f92d15db8
ysh <ysh_79696368@163.com>
2015-04-09 18:35:00 +0800

    ysh
    Signed-off-by: ysh <ysh_79696368@163.com>

--- src/com/example/test/Activity2.java
+++ src/com/example/test/Activity2.java
@@ -1,7 +1,36 @@
 package com.example.test;
 
 import android.app.Activity;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.accessibility.AccessibilityEvent;
 
 public class Activity2 extends Activity {
 
+	@Override
+	public boolean dispatchGenericMotionEvent(MotionEvent ev) {
+		// TODO Auto-generated method stub
+		return super.dispatchGenericMotionEvent(ev);
+	}
+
+	@Override
+	public boolean dispatchKeyEvent(KeyEvent event) {
+		// TODO Auto-generated method stub
+		return super.dispatchKeyEvent(event);
+	}
+
+	@Override
+	public boolean dispatchKeyShortcutEvent(KeyEvent event) {
+		// TODO Auto-generated method stub
+		return super.dispatchKeyShortcutEvent(event);
+	}
+
+	@Override
+	public boolean dispatchPopulateAccessibilityEvent(AccessibilityEvent event) {
+		// TODO Auto-generated method stub
+		return super.dispatchPopulateAccessibilityEvent(event);
+	}
+
+	
+	
 }
